6c8d903584691c8116fb4b108c961573d8f10cb3
leantony <chachaantony@gmail.com>
2017-04-02 20:58:01 +0300

    add function_exists check

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -1,17 +1,19 @@
 <?php
 use Illuminate\Support\Facades\App;
 
-/**
- * Fetch settings
- *
- * @param $key
- * @param null $default
- * @return \Leantony\Settings\SettingsHelper|mixed
- */
-function settings($key = null, $default = null)
-{
-    if ($key === null) {
-        return App::make('settings');
+if (!function_exists('settings')) {
+    /**
+     * Fetch settings
+     *
+     * @param $key
+     * @param null $default
+     * @return \Leantony\Settings\SettingsHelper|mixed
+     */
+    function settings($key = null, $default = null)
+    {
+        if ($key === null) {
+            return App::make('settings');
+        }
+        return App::make('settings')->get($key, $default);
     }
-    return App::make('settings')->get($key, $default);
 }
